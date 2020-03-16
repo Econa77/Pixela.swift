3c2e4589374488696161299547aeb8f6e32bfc9e
Econa77 <s.f.1992.ip@gmail.com>
2020-03-16 14:50:22 +0900

    Add Pixela.xcodeproj

---
+++ Sources/Pixela/Pixela.h
@@ -0,0 +1,17 @@
+//
+//  Pixela.h
+//  Pixela
+//
+//  Created by 古林俊佑 on 2020/03/16.
+//  Copyright © 2020 ShunsukeFurubayashi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Pixela.
+FOUNDATION_EXPORT double PixelaVersionNumber;
+
+//! Project version string for Pixela.
+FOUNDATION_EXPORT const unsigned char PixelaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pixela/PublicHeader.h>
