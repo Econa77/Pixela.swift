4ef564026f2baae038a7f9fdaa7351025f20468c
Econa77 <s.f.1992.ip@gmail.com>
2020-03-16 14:59:40 +0900

    Add APIKit.framework with git submodule

--- Examples/Example-iOS/Example-iOS/ViewController.swift
+++ Examples/Example-iOS/Example-iOS/ViewController.swift
@@ -2,19 +2,17 @@
 //  ViewController.swift
 //  Example-iOS
 //
-//  Created by 古林俊佑 on 2020/03/13.
+//  Created by 古林俊佑 on 2020/03/16.
 //  Copyright © 2020 ShunsukeFurubayashi. All rights reserved.
 //
 
 import UIKit
-import Pixela
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
     }
 
 
